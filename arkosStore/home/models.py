dbfc8e47cfe84e818a12866aeaec160d9c04a60a
Estrella Ángel <estrella.angel.postigo@gmail.com>
2025-11-14 20:02:52 +0100

    feat: add Review model

--- arkosStore/home/models.py
+++ arkosStore/home/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
-
+class Review(models.Model):
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comment = models.TextField(blank=True, null=True)
+    date = models.DateField(auto_now_add=True)
+    def __str__(self):
+        return str(self.rating) # Change when we have appointments
