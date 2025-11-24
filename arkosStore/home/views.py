12dbe34aee679dd3dabbfa3fa4a57f724eb2121e
loubna3 <loufouela@alum.us.es>
2025-11-24 20:38:24 +0100

    feat: new resources view implemented

--- arkosStore/home/views.py
+++ arkosStore/home/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 
-# from django.template import loader
-# from django.http import HttpResponse
-
-# Create your views here.
-
-
 def index(request):
     return render(request, "home/index.html")
 
 
 def contact(request):
     return render(request, "home/contact.html")
+
+def resources_view(request):
+    return render(request, "home/resources.html")
